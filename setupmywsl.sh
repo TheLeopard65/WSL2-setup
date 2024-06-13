37f45d5fd6b2f3699d21f8899244839bfb4a86e8
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-06-13 20:13:48 +0500

    Create setupmywsl.sh

---
+++ setupmywsl.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+if [ ! -n "$1" ]; then
+    echo "SYNTAX : bash setupmywsl.sh <your_root_password>"
+    exit 1
+fi
+sudo su | $1
+
+# Updating and Upgrading for the WSL2
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y burpsuite ghidra wireshark nmap netdiscover exiftool tor
+sudo apt install -y torbrowser-launcher dpkg john network-manager eog aircrack-ng
+sudo apt install -y steghide scapy hashcat python3 theharvester hping3 gobuster
+sudo apt install -y dirbuster autopsy masscan maltego chisel foremost pdf-parser 
+sudo apt install -y ncat reaver wifite netcat-traditional sqlmap zbar-tools
+sudo apt install -y metasploit-framework iptables python3-pip nikto  checksec
+sudo apt install -y ffmpeg mlocate hydra binwalk git python3-pip python3-dev
+sudo apt install -y libssl-dev libffi-dev build-essential
+
+# Installing important pips
+pip install flask flask_socketio bycrypt cryptodome python-socketio numpy tk
+pip install opencv-python websocket-client pyinstaller soundfile sounddevice
+pip install requests pillow pyautogui pynput pwntools yara-python capstone
+pip install leechcorepyc gcsfs s3fs pefile
+
+# Installing PIMPMYKALI
+cd /opt
+git clone https://github.com/Dewalt-arch/pimpmykali.git
+cd pimpmykali
+sudo ./pimpmykali.sh | echo N
+cd ~
+
+# Installing NESSUS
+cd /var/cache/apt/archive/
+wget -O nessus.deb https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/11671/download?i_agree_to_tenable_license_agreement=true
+sudo dpkg -i nessus.deb
+cd ~
+
+# Installing DEEPSOUND
+wget -O /tmp/deepsound.msi http://jpinsoft.net/DeepSound/Download.aspx?Download=LastVersion
+sudo wine msiexec /i /tmp/deepsound.msi
+cd ~
+
+# Installing SSTV Decoder
+cd /opt
+git clone https://github.com/colaclanth/sstv.git
+python3 setup.py install
+cd ~
+
+# Installing Volatility3
+cd /opt
+git clone https://github.com/volatilityfoundation/volatility3.git
+cd volatility3
+python3 setup.py build 
+python3 setup.py install
+cd ~
+
+# Installing Radare2
+cd /opt
+git clone https://github.com/radareorg/radare2
+cd radare2
+bash sys/install.sh
+cd ~
