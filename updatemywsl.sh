57466648f331cc1e7121fc84966fc82457802648
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-06-16 17:15:14 +0500

    Update updatemywsl.sh

--- updatemywsl.sh
+++ updatemywsl.sh
@@ -11,7 +11,7 @@ sudo apt upgrade -y
 sudo apt install -y burpsuite ghidra wireshark nmap netdiscover exiftool tor torbrowser-launcher dpkg john network-manager eog aircrack-ng steghide scapy hashcat python3 theharvester hping3 gobuster dirbuster autopsy masscan maltego chisel foremost pdf-parser  ncat reaver wifite netcat-traditional sqlmap zbar-tools metasploit-framework iptables python3-pip nikto  checksec ffmpeg plocate hydra binwalk git python3-pip python3-dev libssl-dev libffi-dev build-essential libwine winericks
 
 # Installing important pips
-pip install flask flask_socketio bycrypt cryptodome python-socketio numpy tk opencv-python websocket-client pyinstaller soundfile sounddevice requests pillow pyautogui pynput pwntools yara-python capstone leechcorepyc gcsfs s3fs pefile pwnlib
+pip install flask flask_socketio bcrypt pycryptodome python-socketio numpy tk opencv-python websocket-client pyinstaller soundfile sounddevice requests pillow pyautogui pynput pwntools yara-python capstone leechcorepyc gcsfs s3fs pefile pwnlib
 
 # Finel Update and Upgrade
 sudo apt update -y
