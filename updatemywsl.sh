4a3e8d45cd624c5ca9ec6f906a30e9ac3ddc1eab
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-08-04 20:42:56 +0500

    Update updatemywsl.sh

--- updatemywsl.sh
+++ updatemywsl.sh
@@ -7,13 +7,14 @@ fi
 
 # Updating and Upgrading for the WSL2
 apt update -y && apt upgrade -y
-apt install -y openvpn burpsuite ghidra wireshark pip nmap netdiscover libimage-exiftool-perl wafw00f sublist3r tor exploitdb theharvester recon-ng python3-shodan torbrowser-launcher dpkg clamav john network-manager feroxbuster eog aircrack-ng steghide python3-scapy hashcat python3 theharvester hping3 gobuster dirbuster autopsy masscan maltego chisel foremost pdf-parser  ncat reaver wifite netcat-traditional sqlmap zbar-tools metasploit-framework iptables nikto  checksec ffmpeg plocate hydra binwalk git python3-dev libssl-dev libffi-dev build-essential libwine amass openvas-scanner trufflehog bloodhound trivy cme responder wfuzz wpscan pacu kismet-core beef gophish evilginx2 adb smbclient
+apt install -y openvpn wordlists burpsuite ghidra wireshark pip nmap netdiscover libimage-exiftool-perl wafw00f sublist3r tor exploitdb theharvester recon-ng python3-shodan torbrowser-launcher dpkg clamav john network-manager feroxbuster eog aircrack-ng steghide python3-scapy hashcat python3 theharvester hping3 gobuster dirbuster autopsy masscan maltego chisel foremost pdf-parser  ncat reaver wifite netcat-traditional sqlmap zbar-tools metasploit-framework iptables nikto  checksec ffmpeg plocate hydra binwalk git python3-dev libssl-dev libffi-dev build-essential libwine amass openvas-scanner trufflehog bloodhound trivy cme responder wfuzz wpscan pacu kismet-core beef gophish evilginx2 adb smbclient
 
 # Installing important pips
 apt install python3-pip pip pip3
 pip install flask flask_socketio bcrypt pycryptodome python-socketio numpy tk opencv-python websocket-client pyinstaller soundfile sounddevice requests pillow pyautogui pynput pwntools yara-python capstone leechcorepyc gcsfs s3fs pefile pwnlib
 
 # Updating some Important Databases
+gzip -d /usr/share/wordlists/rockyou.txt.gz
 searchsploit -u
 msfupdate
 nmap --script-updatedb
